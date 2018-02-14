389f54e6705906acb93dfaa8f69779f12e0d17a3
Unknown <mariealizee.mechain@gmail.com>
2018-02-14 15:36:32 +0100

    home Service

--- app/home.service.ts
+++ app/home.service.ts
@@ -5,10 +5,12 @@ import { AppConfigService } from './app-config.service';
 @Injectable()
 export class HomeService {
     private token:string;
+    //private proprietaire:Proprietaire;
 
     constructor(private appConfig:AppConfigService,private http:Http){}
 
-    connection(nom_utilisateur:string){
-       return  this.http.get(this.appConfig.uri + '/canal').subscribe(resp => this.token = resp.json());
+    connection(nom:string){
+       this.http.post(this.appConfig.uri + '/auth', { username: nom}).subscribe(resp => this.token = resp.json());
+       //this.proprietaire.token = this.token;
     }
 }
