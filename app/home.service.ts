61efb1458ab75ba23c5302434641879fbc1b79cf
LéoMallet <leomalletdida@gmail.com>
2018-02-14 15:18:14 +0100

    Premiers visuels

--- app/home.service.ts
+++ app/home.service.ts
@@ -4,13 +4,13 @@ import { AppConfigService } from './app-config.service';
 
 @Injectable()
 export class HomeService {
-    private token:string;
+    private token: string;
 
-    constructor(private appConfig:AppConfigService,private http:Http){}
+    constructor(private appConfig: AppConfigService, private http: Http) { }
 
-    connection(nom_utilisateur:string){
-      this.http
-          .post(this.appConfig.uri + '/auth', { username: nom_utilisateur })
-          .subscribe(resp => this.token = resp.json());
+    connection(nom_utilisateur: string) {
+        this.http
+            .post(this.appConfig.uri + '/auth', { username: nom_utilisateur })
+            .subscribe(resp => this.token = resp.json());
     }
 }
